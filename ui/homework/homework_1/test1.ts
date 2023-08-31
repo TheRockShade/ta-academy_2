b4894037a659afc6df032e6d1dd8e14b80ed9d7e
Vladislav Bykov <therockshade@gmail.com>
2023-08-31 12:32:42 +0400

    Complete first homework

--- ui/homework/homework_1/test1.ts
+++ ui/homework/homework_1/test1.ts
@@ -4,30 +4,33 @@
 */
 
 type Person = {
-  occupation?: string;
+    name: string;
+    age: number | 'unknown';
+    isSuperHero?: boolean;
+    occupation: string;
 };
 
 const users: Person[] = [
-  {
-    name: 'Owen Willson',
-    age: 54,
-    occupation: 'Actor',
-  },
-  {
-    name: 'Ben Stiller',
-    age: 57,
-    occupation: 'Filmmaker',
-  },
-  {
-    name: 'Batman',
-    age: 'unknown',
-    isSuperHero: true,
-    occupation: 'vigilante',
-  },
+    {
+        name: 'Owen Willson',
+        age: 54,
+        occupation: 'Actor',
+    },
+    {
+        name: 'Ben Stiller',
+        age: 57,
+        occupation: 'Filmmaker',
+    },
+    {
+        name: 'Batman',
+        age: 'unknown',
+        isSuperHero: true,
+        occupation: 'vigilante',
+    },
 ];
 
 function logPerson(user: Person) {
-  console.log(` - ${user.name}, ${user.age}`);
+    console.log(` - ${user.name}, ${user.age}`);
 }
 
 users.forEach(logPerson);
